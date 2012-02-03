2b71ba626ae043f6e6ecc146efacbe6ae3f5e0d2
Michael Aaron Jansen <mjansen@radarmed.com>
2012-02-03 08:35:41 -0500

    Initial version of QueuedWrapperTarget
    Only the code is done, nothing else has been added to the NLog project.
    Also, I only added it to the .NET 4.0 project.  I'm not familiar with the
    innerds of the NLog project yet, so I'm sure there is plenty more to do,
    like updating the XML Schema to include this wrapper type in the target
    enumeration.

---
+++ src/NLog/Targets/Wrappers/QueuedTargetWrapper.cs
@@ -0,0 +1,68 @@
+﻿namespace NLog.Targets.Wrappers
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using NLog.Common;
+
+    /// <summary>
+    /// A target that buffers log events and sends them in batches to the wrapped target.
+    /// </summary>
+    [Target("QueuedWrapper", IsWrapper = true)]
+    public class QueuedTargetWrapper : WrapperTargetBase
+    {
+        /// <summary>
+        /// Event queue
+        /// </summary>
+        private Queue<AsyncLogEventInfo> _queuedEvents;
+
+        /// <summary>
+        /// Log level at which queued events are written to the log
+        /// </summary>
+        private LogLevel _triggerLevel;
+
+        /// <summary>
+        /// Maximum number of events in queue; excess events are dequeued and discarded
+        /// </summary>
+        [DefaultValue(100)]
+        public int QueueSize { get; set; }
+
+        /// <summary>
+        /// Log level at which queued events are written to the log
+        /// </summary>
+        [DefaultValue("Error")]
+        public string TriggerLevel { get; set; }
+
+        /// <summary>
+        /// Enqueues the log event; if the log level is the trigger level or more severe, all queued events are written
+        /// </summary>
+        /// <param name="logEvent"></param>
+        protected override void Write(AsyncLogEventInfo logEvent)
+        {
+            lock (this.SyncRoot)
+            {
+                _queuedEvents.Enqueue(logEvent);
+
+                if (logEvent.LogEvent.Level >= _triggerLevel)
+                {
+                    while (_queuedEvents.Count > 0)
+                    {
+                        this.WrappedTarget.WriteAsyncLogEvent(_queuedEvents.Dequeue());
+                    }
+                }
+
+                if (_queuedEvents.Count > QueueSize)
+                {
+                    _queuedEvents.Dequeue();
+                }
+            }
+        }
+
+        protected override void InitializeTarget()
+        {
+            base.InitializeTarget();
+            _queuedEvents = new Queue<AsyncLogEventInfo>(QueueSize);
+            _triggerLevel = LogLevel.FromString(TriggerLevel);
+        }
+
+    }
+}
